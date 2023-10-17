e46154439b9102e743bd7becc1556314c146620d
Lenovo 15 <information@com1software.com>
2023-10-17 14:47:30 -0400

    update

--- Go/IP Monitor/main.go
+++ Go/IP Monitor/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-    "io"
-    "log"
-    "net/http"
-    "os"
+	"bufio"
+	"log"
+	"net/http"
 )
 
 func main() {
-    response, err := http.Get("http://com1software.com/")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer response.Body.Close()
-    n, err := io.Copy(os.Stdout, response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Println("Bytes:", n)
+	url := "http://com1software.com"
+	// url = "http://192.168.1.105:8080"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, erra := reader.ReadBytes('\n')
+		if erra != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(string(line))
+	}
+
 }
